0639f9821fa2280d6492dde413c4e05e679cf340
Vikash <vikashpathak041@gmail.com>
2022-07-17 10:05:12 +0530

    start tic tac toe uc2

--- src/com/blz/TicTacToeGame.java
+++ src/com/blz/TicTacToeGame.java
@@ -1,7 +1,28 @@
 package com.blz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
-	public static void main(String[] args) {
-		System.out.println("welcome to tic tac toe");
-	}
-} 
+    public static char inputUser;
+    public static char inputC;
+
+    public static void main(String[] args) {
+        char[] board = new char[10];
+        for (int i = 1; i < 10; i++) {
+            board[i] = ' ';
+        }
+        char inputUser = input();
+        if (inputUser == 'X') {
+            inputC = 'O';
+        } else {
+            inputC = 'X';
+        }
+    }
+
+    public static char input() {
+        System.out.println("Please enter your input in X / O");
+        Scanner in = new Scanner(System.in);
+        char inputU = in.next().charAt(0);
+        return inputU;
+    }
+}
